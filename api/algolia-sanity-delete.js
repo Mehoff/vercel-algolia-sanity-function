278d91bdb9d79ec1f20eb3c1e12f98cf0526d0d7
Mehoff <mixov2000@gmail.com>
2022-12-20 13:04:00 +0200

    try delete

--- api/algolia-sanity-delete.js
+++ api/algolia-sanity-delete.js
@@ -1,9 +1,38 @@
+const algoliasearch = require("algoliasearch");
+
+const algolia = algoliasearch(
+  process.env.ALGOLIA_APPLICATION_ID,
+  process.env.ALGOLIA_ADMIN_API_KEY
+);
+
 const handler = (req, res) => {
   console.log(req.body);
   // TODO: Delete from Algolia
 
+  const algoliaIndex = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+
+  const deleted = req.body.ids.deleted;
+
   res.status = 200;
-  res.json({ message: "Delete", body: req.body });
+
+  if (!deleted) {
+    res.json({ message: "Deleted is undefined", body: req.body });
+    return;
+  }
+
+  algoliaIndex
+    .deleteObjects(deleted)
+    .then((cb) => {
+      console.log(cb);
+      res.json({ message: "Delete", ids: deleted, cb });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ message: "Delete error", error: err });
+    });
+
+  //   res.status = 200;
+  //   res.json({ message: "Delete", body: req.body });
 };
 
 module.exports = handler;
